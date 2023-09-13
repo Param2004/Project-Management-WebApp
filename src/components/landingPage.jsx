f87b6b2762ec482abe2a5a53f6d8f9881651e969
Param sharma <kaushik.param2004@gmail.com>
2023-09-13 16:31:52 +0530

    svg

--- src/components/landingPage.jsx
+++ src/components/landingPage.jsx
@@ -271,11 +271,12 @@ export default function Example() {
               </a>
             </Popover.Group>
             <div className="hidden md:flex items-center justify-end md:flex-1 lg:w-0">
-              <a href='/signin' className="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900">
+              <a href='' onClick={()=>navigate('/signin')} className="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900">
                 Sign in
               </a>
               <a
-                href='/signup'
+                href=''
+                onClick={()=>navigate('/signup')}
                 className="ml-8 whitespace-nowrap inline-flex items-center justify-center bg-gradient-to-r from-purple-600 to-indigo-600 bg-origin-border px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white hover:from-purple-700 hover:to-indigo-700">
                 Sign up
               </a>
