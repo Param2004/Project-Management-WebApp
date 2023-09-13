d5937de3c4413fd87674fbf97095815f005e2fb5
Param sharma <kaushik.param2004@gmail.com>
2023-09-13 14:39:55 +0530

    save

--- src/components/landingPage.jsx
+++ src/components/landingPage.jsx
@@ -1,6 +1,5 @@
 
 import { Fragment } from 'react'
-import {useNavigate} from 'react-router-dom'
 import { Popover, Transition } from '@headlessui/react'
 import {
   AnnotationIcon,
@@ -184,7 +183,6 @@ function classNames(...classes) {
 }
 
 export default function Example() {
-  const navigate = useNavigate();
   return (
     <div className="bg-white">
       <header>
@@ -275,7 +273,6 @@ export default function Example() {
               <a href="" className="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900">
                 Sign in
               </a>
-              <button onClick={() => navigate("./signin")}>hello</button>
               <a
                 href="#"
                 className="ml-8 whitespace-nowrap inline-flex items-center justify-center bg-gradient-to-r from-purple-600 to-indigo-600 bg-origin-border px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white hover:from-purple-700 hover:to-indigo-700"
